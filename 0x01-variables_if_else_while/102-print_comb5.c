aeaf03afb2f799636f8bdf9af09828b1ddc9d6ca
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-08-11 19:45:07 +0300

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,7 +8,7 @@ int main(void)
 {
 int num_0 = 0, num_1 = 0, num_2 = 0, num_3 = 1;
 
-while (num_0 < 10)
+while (num_0 != 9 || num_1 != 9 || num_2 != 10 || num_3 != 0 )
 {
 
 putchar(48 + num_0);
@@ -16,7 +16,7 @@ putchar(48 + num_1);
 putchar(' ');
 putchar(48 + num_2);
 putchar(48 + num_3++);
-if (num_0 != 7 || num_1 != 8 || num_2 != 10)
+if (num_0 != 9 || num_1 != 8 || num_2 != 9 || num_3 != 10)
 {
 putchar(',');
 putchar(' ');
@@ -28,15 +28,25 @@ num_3 = 0;
 }
 if (num_2 == 10)
 {
-if (num_1 != 8)
+if (num_1 < 8)
+{
 num_1++;
+num_3 = num_1 + 1;
+num_2 = num_0;
+}
+else if (num_1 == 8)
+{
+num_1++;
+num_3 == 0;
+num_2 = num_0 + 1;
+}
 else
 {
 num_0++;
 num_1 = 0;
-}
 num_3 = num_1 + 1;
 num_2 = num_0;
+}
 }
 }
 putchar('\n');
