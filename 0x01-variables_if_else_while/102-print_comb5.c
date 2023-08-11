b09daa7251ca4d0fb5ffcd0690e8775a60a6c294
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-08-11 18:52:43 +0300

    102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - the entry pint to function
+ * Return:return 0 if success
+*/
+int main(void)
+{
+int num_0 = 0, num_1 = 0, num_2 = 0,num_3=1;
+
+while (num_0 < 10)
+{
+
+putchar(48 + num_0);
+putchar(48 + num_1);
+putchar(' ');
+putchar(48 + num_2);
+putchar(48 + num_3++);
+if (num_0 != 7 || num_1 != 8 || num_2 != 10)
+{
+putchar(',');
+putchar(' ');
+}
+if (num_3 == 10)
+{
+num_2++;
+num_3 = 0;
+}
+if (num_2 == 10)
+{
+if (num_1 != 8)
+num_1++;
+else
+{
+num_0++;
+num_1 = 0;
+}
+num_3 = num_1+1;
+num_2 = num_0;
+}
+}
+putchar('\n');
+return (0);
+}
