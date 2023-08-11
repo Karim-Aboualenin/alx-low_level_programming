65917d9f324f56cafcbd71dea68ecbaa662b3725
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-08-11 16:19:59 +0300

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - the entry pint to function
+ * Return:return 0 if success
+*/
+int main(void)
+{
+int c = 'a';
+int i;
+
+for (i = 0; i < 26; i++)
+{
+if (c == 'e' || c == 'q')
+c++;
+else
+printf("%c", c++);
+}
+printf("\n");
+return (0);
+}
