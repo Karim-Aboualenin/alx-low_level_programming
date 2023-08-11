bcacade7a6bccc9b3ac7bd3ad66a7b0a7062bb31
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-08-11 17:15:08 +0300

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,7 +9,6 @@ int main(void)
 int num_0 = 0;
 int num_1 = 0;
 
-
 while(num_0 < 9)
 {
 if (num_0 == num_1)
@@ -19,15 +18,15 @@ continue;
 }
 putchar(48 + num_0);
 putchar(48 + num_1++);
-if (num_0 !=8 || num_1 != 10)
+if (num_0 != 8 || num_1 != 10)
 {
 putchar(',');
 putchar(' ');
 }
 if (num_1 == 10)
 {
-num_0++;    
-num_1 = num_0 + 1;  
+num_0++;
+num_1 = num_0 + 1;
 }
 
 }
