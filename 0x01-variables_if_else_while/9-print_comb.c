eefd7019ea8fcd8f0b6297c8c94e21a2aaeba915
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-08-11 16:55:23 +0300

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - the entry pint to function
+ * Return:return 0 if success
+*/
+int main(void)
+{
+int num = 0;
+int c = 'a';
+int i;
+
+for (i = 0; i < 10; i++)
+{
+putchar(48 + num++);
+if (num != 10)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
