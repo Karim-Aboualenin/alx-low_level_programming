7570654fce01ada45c3bf1923a987b7bfe7a68a8
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-08-11 16:50:17 +0300

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - the entry pint to function
+ * Return:return 0 if success
+*/
+int main(void)
+{
+int num = 0;
+int c = 'a';
+int i;
+
+for (i = 0; i < 10; i++)
+{
+putchar(48 + num++);
+}
+for (i = 0; i < 6; i++)
+{
+putchar(c++);
+}
+putchar('\n');
+return (0);
+}
