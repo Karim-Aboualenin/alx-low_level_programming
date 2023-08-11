4da5d76315c92570d73c9fa47a63dfe69ef40744
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-08-11 16:28:44 +0300

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - the entry pint to function
+ * Return:return 0 if success
+*/
+int main(void)
+{
+int num = 0;
+int i;
+
+for (i = 0; i < 10; i++)
+{
+ putchar(48 + num++);
+}
+printf("\n");
+return (0);
+}
