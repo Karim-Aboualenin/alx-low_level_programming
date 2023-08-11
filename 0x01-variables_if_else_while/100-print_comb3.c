2ec127e48b9c3fe9f00639be8dc4aa7023dbe7a6
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-08-11 17:12:59 +0300

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - the entry pint to function
+ * Return:return 0 if success
+*/
+int main(void)
+{
+int num_0 = 0;
+int num_1 = 0;
+
+
+while(num_0 < 9)
+{
+if (num_0 == num_1)
+{
+num_1++;
+continue;
+}
+putchar(48 + num_0);
+putchar(48 + num_1++);
+if (num_0 !=8 || num_1 != 10)
+{
+putchar(',');
+putchar(' ');
+}
+if (num_1 == 10)
+{
+num_0++;    
+num_1 = num_0 + 1;  
+}
+
+}
+putchar('\n');
+return (0);
+}
