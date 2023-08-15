3c8bfea5f3e31b2298403f863c09151be245c98a
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-08-16 01:00:54 +0300

    105

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,10 +9,10 @@ int main(void)
 unsigned long a = 1, a_start, a_end;
 unsigned long b = 2, b_start, b_end;
 int i;
-unsigned long sum , sum_start, sum_end;
+unsigned long sum, sum_start, sum_end;
 unsigned long carry;
 printf("%li, %li", a, b);
-for (i = 0; i < 90; i++)
+for (i = 0; i < 89; i++)
 {
 sum = a + b;
 a = b;
@@ -23,7 +23,7 @@ a_start = a / 100000000000;
 a_end = a % 100000000000;
 b_start = b / 100000000000;
 b_end = b % 100000000000;
-for(i = 0; i < 6; i++)
+for (i = 0; i < 7; i++)
 {
 carry = (a_end + b_end) / 100000000000;
 sum_end = (a_end + b_end) -carry * 10000000000;
