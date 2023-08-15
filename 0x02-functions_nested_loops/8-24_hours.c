17ce702908684314cdcb5e7b3f52e8223a2b7df8
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-08-15 20:27:01 +0300

    8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * Return: nothing
+ */
+void jack_bauer(void)
+{
+int minutes = 0, hours =0;
+while(hours < 24)
+{
+while (minutes < 60)
+{
+_putchar ('0'+ hours / 10);
+_putchar ('0' + hours % 10);
+_putchar(':');
+_putchar ('0' + minutes / 10);
+_putchar ('0' + minutes % 10);
+_putchar ('\n');
+minutes++;
+}
+minutes = 0;
+hours++;
+}
+return (0);
+}
