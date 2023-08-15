63b9f6b47313fb495b1e6acfe73895d0c77d62a7
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-08-15 18:15:14 +0300

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabets 10 times, followed by new line
+ * Return: nothing
+ */
+void print_alphabet(void)
+{
+int c = 'a';
+int i,j;
+for (j = 0; j < 10; j++)
+{
+for (i = 0; i < 26; i++)
+{
+_putchar(c++);
+}
+_putchar('\n');
+}
+}
