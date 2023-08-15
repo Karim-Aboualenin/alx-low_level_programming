64a1d988c8475102f266b78e8bb9f3759dfc98fb
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-08-15 23:29:13 +0300

    102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point to the program
+ * Return: 0
+ */
+int main()
+{
+int a = 1;
+int b = 2;
+int temp;
+int i;
+int sum = a + b;
+printf("%d, %d\n", a, b);
+for (i = 0; i < 50;i++);
+{
+printf ("%d, ", sum);
+temp =sum;
+sum = temp + b;
+b = temp;
+}
+return (0);
+}
