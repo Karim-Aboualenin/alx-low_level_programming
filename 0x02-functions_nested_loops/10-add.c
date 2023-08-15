7719e7fa5875e68abae2b26b1422d0c627aa98b1
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-08-15 22:09:16 +0300

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @a: the first integer
+ * @b: the second integer
+ * Return: the sum of two integers
+ */
+int add(int a, int b)
+{
+return (a+b);
+}
