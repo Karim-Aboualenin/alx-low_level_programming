1e23443e6cb6420a98cb00cc72768869d507cc32
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-08-16 01:19:22 +0300

    7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,12 +7,10 @@
  */
 int print_last_digit(int n)
 {
-if (n > 0)
-{
-_putchar(n % 10 + '0');
-return (n % 10);
-}
-n = -1 * n;
-_putchar(n % 10 + '0');
-return (n % 10);
+if (n < 0)
+		n *= -1;
+
+       	_putchar('0' + (n % 10));
+
+	return (n % 10);
 }
