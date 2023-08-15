a871472e693fd342b044f764fa1f2210726241af
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-08-15 23:19:22 +0300

    101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -4,16 +4,16 @@
  * main - entry point to the program
  * Return: 0
  */
-int main (void)
+int main(void)
 {
 int sum = 0;
 int num = 0;
 while (num < 1024)
 {
-if (num % 3 == 0 ||num  % 5 == 0)
+if (num % 3 == 0 || num  % 5 == 0)
 sum += num;
 num++;
 }
-printf("%d", sum);
+printf("%d\n", sum);
 return (0);
 }
