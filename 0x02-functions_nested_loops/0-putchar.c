5f52067a0aa97d3c2b98195922a5855d32c7dea4
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-08-15 17:49:29 +0300

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+include "main.h"
+  
+/**
+* main - the entry point of the program
+* Return : 0 if success
+*/
+int main(void)
+{
+   _putchar ('_');
+   _putchar ('p');
+   _putchar ('u');
+   _putchar ('t');
+   _putchar ('c');
+   _putchar ('h');
+   _putchar ('a');
+   _putchar ('r');
+  return (0);
+}
