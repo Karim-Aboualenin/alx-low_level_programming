c63efe3d7821b2a6286ccc1589140e095220221f
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-08-17 23:18:37 +0300

    101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,7 +7,21 @@
  */
 void print_number(int n)
 {
-if (n == 0)
+if (n == -2147483648)
+{
+_putchar('-');
+_putchar('2');
+_putchar('1');
+_putchar('4');
+_putchar('7');
+_putchar('4');
+_putchar('8');
+_putchar('3');
+_putchar('6');
+_putchar('4');
+_putchar('8');
+}
+else if (n == 0)
 _putchar('0');
 else
 {
