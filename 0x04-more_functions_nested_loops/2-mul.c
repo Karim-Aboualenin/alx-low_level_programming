624da0b1aa8023f33d57b967d7bbb730e9e02021
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-08-17 18:57:32 +0300

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - add two integers
+ * @a: the first integer
+ * @b: the second integer
+ * Return: the multiplication of two integers
+ */
+int add(int a, int b)
+{
+return (a * b);
+}
