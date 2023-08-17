fd927d189c47d80070014b61c731fb9768d5def5
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-08-17 20:43:10 +0300

    7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints straight line wit n length
+ * @n: the length of the line
+ * Return: nothing
+ */
+void print_line(int n)
+{
+int i;
+
+if (n <= 0)
+_putchar('\n');
+else
+{
+for (i = 0; i < n; i++)
+{
+_putchar('\n');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
