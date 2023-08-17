f2d915013884da26c5cf21e086d6e94c6a50706c
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-08-17 22:08:47 +0300

    10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints straight a triangle
+ * @n: the size of tiangle
+ * Return: nothing
+ */
+void print_diagonal(int n)
+{
+int i, j, k;
+
+if (n <= 0)
+_putchar('\n');
+else
+{
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < n-i; j++)
+{
+_putchar(' ');
+}
+for (k = 0; j <i; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
