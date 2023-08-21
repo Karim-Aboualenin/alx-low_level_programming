e0c33819f949f59e5e56c73c2bb24f7327d86944
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-08-22 02:03:10 +0300

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,10 +1,3 @@
-#include "main.h"
-
-/**
- * rev_string - print string in reverse
- * @s: the string
- * Return: nothing
-*/
 void rev_string(char *s)
 {
 int len = 0;
@@ -15,16 +8,15 @@ while (*s != '\0')
 len++;
 s++;
 }
-s--;
 for (i = 0; i < len; i++)
 {
 s--;
 }
 for (j = 0; j < len / 2; j++)
 {
-temp = *s;
-*s = *(s + len -j -1);
-*(s + len - j -1) = temp;
+temp = *(s);
+*s = *(s + len - 2 * j - 1);
+*(s + len - 2 * j - 1) = temp;
 s++;
 }
 }
