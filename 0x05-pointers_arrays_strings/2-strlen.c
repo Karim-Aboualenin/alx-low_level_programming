817ba18f2334b599871de91ba55068f44db78bfb
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-08-21 19:20:30 +0300

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * int _strlen - return the length of string
+ * @s: the string
+ * Return: the length of the string
+*/
+int _strlen(char *s)
+{
+int len;
+while (s != "\0")
+{
+len++;
+s++;
+}
+}
