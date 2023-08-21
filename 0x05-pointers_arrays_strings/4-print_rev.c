a0b9a75fb5843aa89bd97ef399664530800f4eae
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-08-21 21:12:58 +0300

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,12 +9,14 @@ void print_rev(char *s)
 int len = 0;
 int i;
 char temp;
-len = _strlen(s);
-for (i = 0; i < len / 2; i++)
+while (*s != '\0')
 {
-temp = *s;
-*s = *(s + len -i -1);
-*(s + len -i -1) = temp;
+len++;
+s++;
 }
-_puts(s);
+for (i = 0; i < len; i++)
+{
+_putchar(*s);
+s--;
+}
 }
