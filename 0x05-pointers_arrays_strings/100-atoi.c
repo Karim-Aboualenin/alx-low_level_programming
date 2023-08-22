3a15673e892b76460ea1c83e4c45923b28d496a5
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-08-22 05:19:27 +0300

    100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -22,22 +22,21 @@ return (len);
 
 int _atoi(char *s)
 {
-int start, len, i = 0, neg, sign = 1, ten = 1, num_of_digits = 0, num, temp, j, k,temp_2, l,no_digit = 1;
+int start = 0, len, i = 0, neg = 0, , temp = 0, j = 0, k = 0,
+int sign = 1, ten = 1, num_of_digits = 0, num = 0;
+int l = 0, no_digit = 1, to_int = 0;
 len = _strlen(s);
-printf("len  = %d \n",len);
 for(j = 0;j <= len; j++)
 {
-printf("s[j]  = %c \n",s[j]);
 if (s[j] >= '0' && s[j] <= '9')
 {
 start = j;
 no_digit = 0;
-printf("start  = %d \n",start);
 break;
 }}
 if(no_digit == 1)
 return(0);
-while(i < len)
+while(i < start)
 {
 if (s[i] == '-')
 neg++;
@@ -45,23 +44,18 @@ i++;
 }
 if (neg % 2 != 0)
 sign = -1;
-printf("sign  = %d \n",sign);
 temp = start;
 while((s[temp] >= '0' && s[temp] <='9') && s[temp] != '\0')
 {
 num_of_digits++;
 temp++;
 }
-printf("num_of_digits  = %d \n",num_of_digits);
-temp_2=num_of_digits;
 for (l = 1;l < num_of_digits; l++)
 ten *= 10;
-printf("ten = %d \n",ten);
-for (k = 0; k < temp_2; k++)
+for (k = 0; k < num_of_digits; k++)
 {
-printf("s[start]  = %c \n",s[1]);
-num += (s[start] - '0') * ten;
-printf("num = %d\n",num);
+to_int = s[start] - '0';
+num += to_int * ten;
 ten /= 10;
 start++;
 }
