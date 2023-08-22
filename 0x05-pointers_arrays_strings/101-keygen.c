597ab2306b0f2b09bb2992e1da9f7af861d630f5
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-08-22 20:16:12 +0300

    101

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,5 +8,22 @@
  */
 int main(void)
 {
+int pass[64];
+int i, sum = 0, n;
+
+srand (time(null));
+for (i = 0; i < 64; i++)
+{
+pass[i] = rand() % 126;
+sum += pass[i];
+putchar(pass[i]);
+if ((2772 - sum)  < 126)
+{
+n = 2772 - sum;
+sum += n;
+putchar(n);
+break;
+}
+}
 return (0);
 }
