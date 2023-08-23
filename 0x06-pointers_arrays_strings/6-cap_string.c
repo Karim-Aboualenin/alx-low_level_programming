3c3ed72f3256e2445d3396d8abb5270985aa091b
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-08-23 20:49:05 +0300

    6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _strlen - return the length of string
+ * @s: the string
+ * Return: the length of the string
+*/
+int _strlen(char *s)
+{
+int len = 0;
+while (*s != '\0')
+{
+len++;
+s++;
+}
+return (len);
+}
+
+/**
+ * cap_string - a function that capitalizes all words of 
+ * a string.
+ * @s: string
+ * Return: a pointer to destination
+ */
+char *cap_string(char *s)
+{
+int i, len;
+len = _strlen(s);
+for (i = 0; i < len; i++)
+{
+if ((s[i] == ',' || s[i] == ';' || s[i] == '.'
+|| s[i] == '!' || s[i] == '?' || s[i] == '"' 
+|| s[i] == '(' || s[i] == ')'|| s[i] == '{' 
+|| s[i] == '}' || s[i] == ' '
+|| s[i] == '\n' || s[i] == '\t')
+&& (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
+s[i+1] = s[i+1] - 32;
+}
+return (s);
