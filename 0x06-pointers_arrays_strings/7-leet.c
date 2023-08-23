30d6808939e8ca18437881e1582b095dd11141ae
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-08-23 22:24:43 +0300

    7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,36 +1,19 @@
 #include "main.h"
 
 /**
- * _strlen - return the length of string
- * @s: the string
- * Return: the length of the string
-*/
-int _strlen(char *s)
-{
-int len = 0;
-while (*s != '\0')
-{
-len++;
-s++;
-}
-return (len);
-}
-
-/**
  * leet - a function that encodes a string into 1337
  * @s: the string
  * Return: a pointer to the string
 */
 char *leet(char *s)
 {
-int i, j, len;
+int i, j;
 char a[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 char b[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
-len = _strlen(s);
-for (i = 0; i < len; i++)
+for (i = 0; s[i] != '\0'; i++)
 {
-for (j = 0; j < 11; j++)
+for (j = 0; j != '\0'; j++)
 {
 if (s[i] == a[j])
 s[i] = b[j];
