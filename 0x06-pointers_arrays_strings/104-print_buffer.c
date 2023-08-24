48ccc38e394d0710442f955d05e5e68320a21f82
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-08-25 00:12:57 +0300

    104

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -12,6 +12,10 @@ void print_buffer(char *b, int size)
 int i, j = 0, k = 0, temp, l = 0, temp_2;
 int address = 0x00;
 
+if (size <= 0)
+printf("\n");
+else
+{
 for (i = 0; i <= size; i = i + 10)
 {
 printf("%.8x:", address + l++ * 0x0a);
@@ -39,6 +43,5 @@ printf(".");
 }
 printf("\n");
 }
-if (size <= 0)
-printf("\n");
+}
 }
