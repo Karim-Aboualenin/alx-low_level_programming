5ff72bd1b0ce9cf834781f2289c6f1cd75b4a46a
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-08-28 19:33:25 +0300

    3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,8 +3,7 @@
  * _strspn - a function that gets the length of a prefix substring.
  * @s: the string
  * @accept: the accepted bytes
- *
- * Returns: the number of bytes in the initial segment
+ * Return: the number of bytes in the initial segment
  * of s which consist only of bytes from accept
  */
 
