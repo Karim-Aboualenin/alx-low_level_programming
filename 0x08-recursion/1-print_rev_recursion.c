30ab4a7a9558f4ffd7444f64c4642455a2c3a9fe
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-08-30 16:20:41 +0300

    1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - print string in reverse
+ * @s: the string
+ * Return: nothing
+*/
+void print_rev(char *s)
+{
+int len = 0;
+int i;
+while (*s != '\0')
+{
+len++;
+s++;
+}
+s--;
+for (i = 0; i < len; i++)
+{
+_putchar(*s);
+s--;
+}
+_putchar('\n');
+}
