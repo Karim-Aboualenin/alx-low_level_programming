9fd0015853e25e0a616ad1c8b63db8a658e6ed78
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-08-30 20:26:49 +0300

    5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,17 +1,4 @@
 #include "main.h"
-/**
- *  _sqrt_recursion- a function that returns the natural
- * square root of a number.
- * @n: the number
- * Return: the natural square root of a number.
-*/
-
-int _sqrt_recursion(int n)
-{
-if (n <= 0)
-return (-1);
-return (find_root(n, 1));
-}
 
 /**
  *  find_root- a function that check if a number have root and return it
@@ -27,4 +14,19 @@ return (-1);
 if (r * r == n)
 return (r);
 return (find_root(n, r + 1));
+}
+
+
+/**
+ *  _sqrt_recursion- a function that returns the natural
+ * square root of a number.
+ * @n: the number
+ * Return: the natural square root of a number.
+*/
+
+int _sqrt_recursion(int n)
+{
+if (n <= 0)
+return (-1);
+return (find_root(n, 1));
 }
