3da6c45bbbb96d38a4fe378069fb79bb7d24ce34
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-09-01 20:27:33 +0300

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ * @argc: the counter for arguments
+ * @argv: an array of strings for arguments
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argv;
+printf("%d\n", argc);
+return (0);
+}
