4690abe97d4f55edfca9d17627baa8eb57ec8cd6
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-09-01 20:36:59 +0300

    3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ * @argc: the counter for arguments
+ * @argv: an array of strings for arguments
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+int mul;
+if (argc != 3)
+printf("Error");
+else
+{
+mul = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n",mul);
+}
+return (0);
+}
