b3a659e972d62dbffedafa12ec6ed7c9362b054a
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-09-01 20:26:19 +0300

    0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,13 +3,14 @@
 
 /**
  * main - check the code
- *
+ * @argc: the counter for arguments
+ * @argv: an array of strings for arguments
  * Return: Always 0.
  */
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
 (void) argc;
-printf("%s\n",argv[0]);    
-return 0;
+printf("%s\n", argv[0]);
+return (0);
 }
