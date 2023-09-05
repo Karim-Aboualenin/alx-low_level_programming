501e30a57c28666c1c392cfbf0b28d7a816f7afa
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-09-06 02:42:15 +0300

    3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,15 +15,23 @@ int **arr,i,j;
   
 if (width <= 0 || height <= 0)
 return (NULL);
-arr = malloc(width *height *sizeof(int));
+arr = malloc(malloc(sizeof(int) * height););
 if (arr == NULL)
 return (NULL);
 for (i = 0; i < height; i++)
 {
 for (j = 0; j < width; j++)
 {
-arr[i][j] = 0;
+arr[i] = malloc(sizeof(int) * width);
+if (arr[i] == NULL)
+{
+for (j = 0; j < i; j++)
+free(arr[j]);
+free(arr);
+return (NULL);
 }
+for (j = 0; j < width; j++)
+arr[i][j] = 0;
 }
 return (arr);
 }
