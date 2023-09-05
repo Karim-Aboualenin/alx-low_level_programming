ef9906b728968425687e2024d85c64f7f0c13d0f
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-09-06 02:14:46 +0300

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,20 +19,24 @@ return (len);
 }
 
 /**
- * str_concat - a function that concatenates two strings.
- * @s1: the first sting
+ * check_Null- handle null strings
+ * @s1: the first string
  * @s2: the second string
- * Return: pointer should point to
- * a newly allocated space in memory which contains
- * the contents of s1, followed by the contents of s2, and null terminated
- */
-char *str_concat(char *s1, char *s2)
+ * Return: the pointer if it's null
+*/
+
+char *check_Null(char *s1, char *s2)
 {
-int len1 = _strlen(s1), len2 = _strlen(s2), i = 0,j = 0, k = 0;
-char *s;
+int len1 , len2
+if (s1 == NULL)
+s1 = ""
+if (s2 == NULL)
+s2 = ""
+len1 = _strlen(s1);
+len2 = _strlen(s2);
 if (len2 == 0)
 {
-s = malloc((len1) *sizeof(char) + 1);
+s = malloc((len1) * sizeof(char) + 1);
 if (s == NULL)
 return (NULL);
 for (i = 0; i < len1; i++)
@@ -52,6 +56,25 @@ s[i] = s2[i];
 }
 return (s);
 }
+return (Null);
+}
+/**
+ * str_concat - a function that concatenates two strings.
+ * @s1: the first sting
+ * @s2: the second string
+ * Return: pointer should point to
+ * a newly allocated space in memory which contains
+ * the contents of s1, followed by the contents of s2, and null terminated
+ */
+char *str_concat(char *s1, char *s2)
+{
+int len1 , len2 , i = 0, j = 0, k = 0;
+char *s;
+s = check_Null(s1, s2);
+if (s == NULL)
+{
+len1 = _strlen(s1);
+len2 = _strlen(s2);
 s = malloc((len1 + len2) *sizeof(char) + 1);
 if (s == NULL)
 return (NULL);
@@ -62,6 +85,7 @@ s[i] = s1[i];
 for (j = i; j < len2 + i; j++)
 {
 s[j] = s2[k++];
+}
 }
 return (s);
 }
