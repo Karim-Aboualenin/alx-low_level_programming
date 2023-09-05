f297b69bec2cbddbec789d5845f7753c01389d60
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-09-06 01:06:34 +0300

    1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -33,7 +33,7 @@ int len;
 if (str == NULL)
 return (NULL);
 len = _strlen(str);
-s = malloc(len *sizeof(char));
+s = malloc(len *sizeof(char) + 1);
 if (s == NULL)
 return (NULL);
 while (str[i] != '\0')
