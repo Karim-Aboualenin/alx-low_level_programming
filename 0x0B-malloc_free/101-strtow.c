1353d66061fc2d218958a7cb2271cf8d0af5e992
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-09-07 01:15:57 +0300

    10

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -24,43 +24,44 @@ return (len);
  * Return: returns a pointer to an array of strings (words)
  */
 
-char **strtow(char *str){
+char **strtow(char *str)
+{
 char **s;
 int i = 0, counter = 0 , index = 0, j = 0, start, len, k = 0, l = 0;
 if (str == NULL || str[0] == '\0')
 return NULL;
-while (str[i] != '\0'){
+for (i = 0; str[i]; i++){
 if (str[i] != ' ' && (str[i - 1] == ' ' || i == 0))
 counter++;
-i++;
 }
 if (counter == 0)
 return (NULL);
 s = malloc(sizeof(char *) * (counter + 1));
 if (s == NULL)
 return (NULL);
-while (str[j] != '\0')
+s[counter] = NULL;
+for (j = 0; str[j]; j++)
 {
 if (str[j] != ' ' && (str[j - 1] == ' ' || j == 0))
 {
 start = j;
 len = 0;
-while (str[j] != ' ' && str[j] != '\0'){
+while (str[j] != ' ' && str[j])
+{
 len ++;
 j++;
 }
-s[index] = malloc(len *sizeof(char*) + 1);
+s[index] = malloc(sizeof(char) * (len + 1));
 if (s[index] == NULL){
-free(s);
 for (k = 0 ;k < index; k++)
 free (s[k]);
+free(s);
 return(NULL);
 }
 for (l = 0; l < len; l++)
 s[index][l] = str[start + l];
 s[index][l] = '\0';
 index++;
-j++;
 }}
 return (s);
 }
