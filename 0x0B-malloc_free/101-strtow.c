d350d617e21c0f331d14271ac5dd8cb0bcf41858
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-09-06 03:08:33 +0300

    101

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * strtow - a function that splits a string into words.
+ * @str:  a string
+ *
+ * Return: returns a pointer to an array of strings (words)
+ */
+
+char **strtow(char *str)
+{
+return (NULL);
+}
