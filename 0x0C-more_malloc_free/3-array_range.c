a93e356f77d1538e54e494880c4151baf2a2e11c
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-09-07 23:48:11 +0300

    3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,7 +13,7 @@ int i, j = 0;
 int *arr;
 if (min > max)
 return (NULL);
-arr = malloc((max - min + 1) *sizeof(int));
+arr = malloc((max - min + 1) * sizeof(int));
 if (arr == NULL)
 return (NULL);
 for (i = min; i <= max; i++)
