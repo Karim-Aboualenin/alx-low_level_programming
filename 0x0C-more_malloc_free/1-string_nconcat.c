67b932da3db64402eb7558fc0b2a9ef101a7401c
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-09-07 22:09:14 +0300

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -95,8 +95,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 unsigned int len1, len2, i = 0, j = 0, k = 0;
 char *s;
 s = check_Null(s1, s2, n);
-if (s == NULL)
-return(NULL);
 len1 = _strlen(s1);
 len2 = _strlen(s2);
 if (n >= len2)
