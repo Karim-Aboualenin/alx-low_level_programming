935e5e2ce988f5c780745d30f24ecc31f8662180
Karim-Aboualenin <karim72727272@yahoo.com>
2023-09-12 00:02:16 +0300

    5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+
+/**
+ * new_dog - a function that frees dogs.
+ * @d: the dog
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+free (d);
+free(d-> name);
+free(d-> age);
+free(d-> owner);
+}
