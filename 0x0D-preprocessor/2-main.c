a5998a7336bc8a9b3319ba962164aab2b1cf9ae3
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-09-11 20:47:10 +0300

    2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -7,20 +7,13 @@
  */
 int main(void)
 {
-    char *str;
-    int i = 0;
-<<<<<<< Updated upstream
-    *str = __FILE__;
-    while(str[i] != '\0')
-    {
-        _putchar(str[i]);
-=======
-    str = __FILE__;
-    while(str[i] != '\0')
-    {
-        _putchar(str[i]);
-        i++;
->>>>>>> Stashed changes
-    }
-    _putchar('\n');
+char *str;
+int i = 0;
+str = __FILE__;
+while(str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
 }
