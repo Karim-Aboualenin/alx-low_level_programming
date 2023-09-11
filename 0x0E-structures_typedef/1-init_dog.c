6981eb3d3016a47f0508f7f594eeec7b39214d26
Karim-Aboualenin <karim72727272@yahoo.com>
2023-09-11 22:55:00 +0300

    1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -6,8 +6,7 @@
  * @name: the name of the dog
  * @age: the age of the dog
  * @owner: the owner of the dog
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: the structure after intialized
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,11 @@
+#include "dog.h"
+/**
+ * print_dog - a function that prints a struct dog
+ * @d: The structure
+ * Return: nothing
+ */
+
+void print_dog(struct dog *d)
+{
+
+}
