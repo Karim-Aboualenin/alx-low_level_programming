6dd3daeca03da2b47094b6a117999f2d81ce14ec
Karim-Aboualenin <karim72727272@yahoo.com>
2023-09-11 23:02:22 +0300

    2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,14 +12,14 @@ if (d != NULL)
 if (d->name == NULL)
 printf("Name: (nil)");
 else
-printf("Name: %s", d->name);
+printf("Name: %s\n", d->name);
 if (d->age == 0)
 printf("age: (nil)");
 else
-printf("age: %f", d->age);
+printf("age: %f\n", d->age);
 if (d->owner == NULL)
 printf("owner: (nil)");
 else
-printf("owner: %s", d->owner);
+printf("owner: %s\n", d->owner);
 }
 }
