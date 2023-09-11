ffb4e602a3a08cb6afd5768ddcaf412e6f57c090
Karim-Aboualenin <105166692+Karim-Aboualenin@users.noreply.github.com>
2023-09-11 20:33:05 +0300

    2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+    int i = 0;
+    *str = __FILE__;
+    while(str[i] != '\0')
+    {
+        _putchar(str[i]);
+    }
+    _putchar('\n');
+}
