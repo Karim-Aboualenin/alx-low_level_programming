84df2f2e86d3ad6aec41a986add7bb2ea3ba2d6e
Karim-Aboualenin <karim72727272@yahoo.com>
2023-09-12 00:05:45 +0300

    5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,13 +1,16 @@
 #include "dog.h"
 
 /**
- * new_dog - a function that frees dogs.
+ * free_dog - a function that frees dogs.
  * @d: the dog
  * Return: nothing
  */
 void free_dog(dog_t *d)
 {
+if (d != NULL)
+{
 free (d);
 free(d-> name);
 free(d-> owner);
+}
 }
