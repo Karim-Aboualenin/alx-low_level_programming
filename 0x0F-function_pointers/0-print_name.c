d9ff5e291fda002b05be5b810ce25f2b1bdea373
Karim-Aboualenin <karim72727272@yahoo.com>
2023-09-13 14:03:14 +0300

    0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name of the person
+ * @f: pointer to function to print name
+ * Return: Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+void (*ptr)(char *) = f;
+ptr(name);
+}
