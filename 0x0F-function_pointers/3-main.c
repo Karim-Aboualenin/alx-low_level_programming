6e2df968c1e15366ae9935c786a2aabcff3dc73c
Karim-Aboualenin <karim72727272@yahoo.com>
2023-09-13 23:21:16 +0300

    3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,22 +13,23 @@ int (*operate)(int , int);
 int num1 ,num2;
 int result;
 char* op;
-printf("%d", argc);
+printf("argc = %d\n", argc);
 if (argc != 4)
 {
-printf ("Errorsgvhx\n");
+printf ("Error\n");
 exit(98);
 }
 op = argv[2];
 num1 = atoi(argv[1]);
 num2 = atoi(argv[3]);
 operate = get_op_func(op);
+printf("printf("argc = %d\n", operate)");
 if (operate == 0)
 {
 printf ("Error\n");
 exit(99);
 }
 result = operate(num1, num2);
-printf("%d", result);
+printf("%d\n", result);
 return (0);
 }
