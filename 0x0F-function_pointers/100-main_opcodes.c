41f7ca8068164c43e3ef06100d8cd44477e52594
Karim-Aboualenin <karim72727272@yahoo.com>
2023-09-14 00:00:35 +0300

    100

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,33 @@
+#include "function_pointers.h"
+
+/**
+ * main - check the code
+ * @argc: thr number of arguments
+ * @argv: the array of arguments
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+int byte;
+int i = 0;
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
+byte = atoi(argv[1]);
+if (byte < 0)
+{
+printf("Error\n");
+exit(2);
+}
+while (i < byte)
+{
+if (i == 0)
+printf("%02x", *(char *)main);
+printf(" %02x", *((char *)main + i));
+}
+printf("\n");
+return (0);
+}
