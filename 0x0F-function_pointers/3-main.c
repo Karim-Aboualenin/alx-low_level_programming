b8d344d6ff2c510acd0d4511c7e9702cc27e78b5
Karim-Aboualenin <karim72727272@yahoo.com>
2023-09-13 23:15:41 +0300

    3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,8 +19,8 @@ printf ("Error\n");
 exit(98);
 }
 op = argv[2];
-num1 = atoi(*argv[1]);
-num2 = atoi(*argv[3]);
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
 operate = get_op_func(op);
 if (operate == 0)
 {
