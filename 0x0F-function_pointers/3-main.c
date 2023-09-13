9a176d2570c7a1427902a526bab55e8fd6b49f81
Karim-Aboualenin <karim72727272@yahoo.com>
2023-09-13 17:10:00 +0300

    3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+
+**
+ * main - check the code
+ * argc: thr number of arguments
+ * argv: the array of arguments
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+int num1 ,num2;
+char op;
+if (argc > 4)
+{
+printf ("Error");
+exit(98);
+}
+op = argv[2];
+if (op != '+' && op != '-' && op != '*' && argv[2] != '/')
+{
+printf ("Error");
+exit(99)
+}
+num1 = argv[1];
+num2 = argv[2];
+get_op_func(op))(num1, num2);
+return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,68 @@
+#include "3-calc.h"
+
+/**
+ * op_add - add two number
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: returns the sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - sub two number
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: returns the difference between a and b
+ */
+
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ * op_mul - multiply two number
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: returns the product of a and b
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ * op_div - divide two number
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: returns the division of a and b
+ */
+int op_div(int a, int b)
+{
+if (b == 0)
+{
+exit (100);
+}
+return (a / b);
+}
+/**
+ * op_mod - returns the remainder of the division of a by two numbers
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: returns the remainder of the division of a and b
+ */
+int op_mod(int a, int b)
+{
+if (b == 0)
+{
+exit (100);
+}
+return (a % b);
+}
