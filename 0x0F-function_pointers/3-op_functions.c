3ad8435f624ced3a7a0a682206c108575032414f
Karim-Aboualenin <karim72727272@yahoo.com>
2023-09-13 23:35:10 +0300

    3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -47,6 +47,7 @@ int op_div(int a, int b)
 {
 if (b == 0)
 {
+printf("Error\n");
 exit (100);
 }
 return (a / b);
@@ -62,6 +63,7 @@ int op_mod(int a, int b)
 {
 if (b == 0)
 {
+printf("Error\n");
 exit (100);
 }
 return (a % b);
