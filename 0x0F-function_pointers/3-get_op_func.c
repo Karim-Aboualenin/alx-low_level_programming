6518e7353c4ff6904a30ffc31b3dd17da343a1a0
Karim-Aboualenin <karim72727272@yahoo.com>
2023-09-13 23:39:52 +0300

    3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -3,7 +3,7 @@
 
 /**
  * get_op_func - selects the correct function
- * to perform the operation asked by the user. 
+ * to perform the operation asked by the user.
  * @s: the operator passed as argument to the program
  *
  * Return: returns a pointer to the function that
@@ -12,17 +12,17 @@
 int (*get_op_func(char *s))(int, int)
 {
 op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
 int i = 0;
 while (i < 5)
 {
-if (strcmp(s,ops[i].op) == 0)
+if (strcmp(s, ops[i].op) == 0)
 {
 return (ops[i].f);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,20 +2,20 @@
 
 /**
  * main - check the code
- * argc: thr number of arguments
- * argv: the array of arguments
+ * @argc: thr number of arguments
+ * @argv: the array of arguments
  * Return: Always 0.
  */
 
 int main(int argc, char *argv[])
 {
-int (*operate)(int , int);
-int num1 ,num2;
+int (*operate)(int, int);
+int num1, num2;
 int result;
-char* op;
+char *op;
 if (argc != 4)
 {
-printf ("Error\n");
+printf("Error\n");
 exit(98);
 }
 op = argv[2];
@@ -24,7 +24,7 @@ num2 = atoi(argv[3]);
 operate = get_op_func(op);
 if (operate == 0)
 {
-printf ("Error\n");
+printf("Error\n");
 exit(99);
 }
 result = operate(num1, num2);
