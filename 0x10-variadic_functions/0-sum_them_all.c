3549db32111d75d5df51c515ae56a55700cf992d
Karim-Aboualenin <karim72727272@yahoo.com>
2023-09-14 20:36:43 +0300

    0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include "variadic_functions.h"
+
+/**
+ * print_dog - a function that prints a struct dog
+ * @d: The structure
+ * Return: nothing
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+unsigned int i;
+int sum = 0;
+va_list args;
+va_start(args, n);
+for (i = 0; i < n; i++)
+{
+sum += va_arg(args, int);
+}
+return (sum);
+}
