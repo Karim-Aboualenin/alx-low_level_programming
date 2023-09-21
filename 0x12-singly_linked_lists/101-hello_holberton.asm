102becd157c952ad35b2aa49fa3b7d6960fdb4b7
Karim-Aboualenin <karim72727272@yahoo.com>
2023-09-21 22:24:16 +0300

    101

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,14 @@
+message db "Hello, Holberton", 0
+global main
+
+main:
+        mov rdi, 1
+        mov rax, 1
+        mov rsi, message
+        mov rdx, 17
+        syscall
+        mov rax, 60
+        mov rdi, 0
+        syscall
+
+
