62f2f2fcdc1b8bb99edc8ccfc032bcf7d3066019
Karim-Aboualenin <karim72727272@yahoo.com>
2023-09-21 22:09:30 +0300

    100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,13 @@
 #include "lists.h"
 void print_hare_and_Tortoise(void) __attribute__((constructor));
 /**
- * print_hare_and_Tortoise - a function that prints You're beat! and 
+ * print_hare_and_Tortoise - a function that prints You're beat! and
  * yet, you must allow,\nI bore my house upon my back!\n before
  * the main function is executed.
  * Return: Nothing
 */
 void print_hare_and_Tortoise(void)
 {
-printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
