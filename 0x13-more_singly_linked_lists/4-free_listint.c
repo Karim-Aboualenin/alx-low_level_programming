e74920df5f3dac622aee8ce3a191601e6e44b594
Karim-Aboualenin <karim72727272@yahoo.com>
2023-09-25 20:49:56 +0300

    4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list -  a function that frees a list_t list.
+* @head: the list
+* Return: the number of nodes
+*/
+void free_listint(listint_t *head)
+{
+listint_t *node;
+if (head == NULL)
+return;
+while (head != NULL)
+{
+node = head->next;
+free(head);
+head = node;
+}
+}
