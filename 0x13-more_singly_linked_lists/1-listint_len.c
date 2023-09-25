2171e73e298afd6b8ae2a74f621ec362d3df34dc
Karim-Aboualenin <karim72727272@yahoo.com>
2023-09-25 20:22:46 +0300

    1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * list_len -  a function that returns the number of elements in a
+ * linked list_t list.
+* @h: the list
+* Return: the number of nodes
+*/
+size_t listint_len(const listint_t *h)
+{
+size_t n = 0;
+while (h != NULL)
+{
+h = h->next;
+n++;
+}
+return (n);
+}
