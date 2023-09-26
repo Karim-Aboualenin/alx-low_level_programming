e590f26a44a6202846daad8773df3a9e3336ac56
Karim-Aboualenin <karim72727272@yahoo.com>
2023-09-26 21:41:30 +0300

    101

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe -  function that prints all the elements of a listint_t list.
+* @h: the list
+* Return: the number of nodes
+*/
+size_t print_listint_safe(const listint_t *head)
+{
+int difference;
+size_t num = 0;
+while (head != NULL)
+{
+difference = head - head->next;
+printf("[%p] ", (void *)head);
+printf("%d\n", head->n);
+if (difference > 0)
+{
+head = head->next;
+num++;
+}
+else
+{
+head = head->next;
+printf("-> [%p] ", (void *)head);
+printf("%d\n", head->n);
+break;
+}
+}
+return (num);
+}
