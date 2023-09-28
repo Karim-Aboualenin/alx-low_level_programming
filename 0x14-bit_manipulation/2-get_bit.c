fb14d1d496c7b7be5fb6dc39c27dff4d685d2c23
Karim-Aboualenin <karim72727272@yahoo.com>
2023-09-28 23:47:24 +0300

    2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -6,15 +6,16 @@
  * @n: the number
  * @index: the index
  * Return:the bit
- * 
 */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
 unsigned long int number;
+if (index > 64)
+return (-1);
 number = n >> index;
 if (number % 2  == 1)
-return(1);
+return (1);
 else
 return (0);
 }
