62172a6058d931f269268220756befccc60bc825
Karim-Aboualenin <karim72727272@yahoo.com>
2023-09-28 22:36:26 +0300

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -24,13 +24,14 @@ return (result);
 
 unsigned int binary_to_uint(const char *b)
 {
-int i = 0, count, j = 0;
-double k;
-unsigned int num = 0, binary;
+/*int i = 0, count, j = 0;
+double k;*/
+unsigned int num = 0;/*, binary;*/
 if (b == NULL)
 {
 return (0);
 }
+/*
 while (b[i] != '\0')
 {
 if (b[i] != '0' && b[i] != '1')
@@ -43,6 +44,6 @@ for (k = count - 1; k >= 0; k--)
 binary = b[j] - 48;
 num += binary * (unsigned int)pow(2, k);
 j++;
-}
+}*/
 return (num);
 }
