22b0fd3f9ef90f650c36c7ff176c1c2ce3871742
Karim-Aboualenin <karim72727272@yahoo.com>
2023-10-03 00:38:42 +0300

    0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,22 +16,25 @@ return (0);
 message = malloc(sizeof(char) * letters);
 if (message == NULL)
 return (0);
-fd = open(filename, O_RDONLY, 600);
+fd = open(filename, O_RDONLY);
 if (fd == -1)
 {
 free(message);
+close(fd);
 return (0);
 }
 rd = read(fd, message, letters);
 if (rd == -1)
 {
 free(message);
+close(fd);
 return (0);
 }
-wr = write(1, message, rd);
+wr = write(STDOUT_FILENO, message, rd);
 if (wr == -1 || wr != rd)
 {
 free(message);
+close(fd);
 return (0);
 }
 close(fd);
