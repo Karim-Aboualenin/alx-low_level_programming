35703e9b85af0445e2cf86b90339516e3347d02c
Karim-Aboualenin <karim72727272@yahoo.com>
2023-11-09 12:38:17 +0200

    Update 5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,5 +1,23 @@
 #include "lists.h"
 /**
+ * dlistint_len - a function that returns the number of
+ * elements of a dlistint_t list.
+ * @h: the head of list
+ * Return: the number of nodes
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+size_t num = 0;
+const dlistint_t *current = h;
+while (current != NULL)
+{
+num++;
+current = current->next;
+}
+return (num);
+}
+
+/**
  * get_dnodeint_at_index - a function that returns the nth node of a
  * dlistint_t linked list.
  * @head: the head of list
