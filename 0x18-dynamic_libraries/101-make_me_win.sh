2ddc2cb6a6acfda7b7bba2c214318a1a9a6ba49d
Karim_Aboualenin <karim72727272@yahoo.com>
2023-11-13 11:18:04 -0800

    100

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/monoprosito/Karim-Aboualenin/alx-low_level_programming/blob/master/0x18-dynamic_libraries/YouWin.so
-export LD_PRELOAD="$PWD/../YouWin.so"
+wget -P .. https://raw.githubusercontent.com/monoprosito/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
 
